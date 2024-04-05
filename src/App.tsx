8816539c59b46d6abdd4321392fa3de6d7a06a63
Mohamed Islem Ayari <mohamed-islem.ayari@cloudpilots.com>
2024-04-05 02:08:15 +0200

    Add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import "./App.css";
-import  LandingPage  from "./Pages/LandingPage/LandingPage";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+
+const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `    body {
+        background: linear-gradient(to right, rgb(0, 0, 0), #010d45);
+        color: rgb(55, 255, 255);
+      }`,
+    },
+  },
+});
 
 function App() {
-  return <>
-  <LandingPage/>
-  </>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <LandingPage />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
