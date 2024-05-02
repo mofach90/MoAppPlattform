11e0b205948097dce02b90dd0562316ea92f2712
Mohamed Islem Ayari <mohamed-islem.ayari@cloudpilots.com>
2024-05-02 04:26:31 +0200

    Working on the lauguage

--- src/Pages/LandingPage/LPcomponents/Footer/Footer.tsx
+++ src/Pages/LandingPage/LPcomponents/Footer/Footer.tsx
@@ -1,4 +1,16 @@
-import { Breadcrumbs, Link as LinkButton, Stack } from "@mui/material";
+import LanguageIcon from "@mui/icons-material/Language";
+import {
+  Avatar,
+  Box,
+  Breadcrumbs,
+  FormControl,
+  InputLabel,
+  Link as LinkButton,
+  MenuItem,
+  Select,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -18,6 +30,7 @@ const Footer = () => {
         flexDirection={"row"}
         alignItems={"center"}
         justifyContent={"end"}
+        // bgcolor={"white"}
       >
         <Breadcrumbs separator={"|"} sx={{ color: "white" }}>
           <Link to="/about" style={{ color: "white", fontSize: "1rem" }}>
@@ -41,9 +54,124 @@ const Footer = () => {
             User Agreement
           </Link>
         </Breadcrumbs>
+        <Box
+          width={180}
+          border={"1px solid"}
+          height={40}
+          mr={2}
+          ml={2}
+          borderRadius={10}
+          display={"flex"}
+          flexDirection={"row"}
+          alignItems={"center"}
+          overflow={"hidden"}
+        >
+          <LanguageIcon sx={{ ml: 1 }} />
+          <FormControl
+            fullWidth
+            sx={{
+              "&.MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+            }}
+          >
+            <InputLabel
+              id="Language"
+              sx={{ color: "#ffffff", bgcolor: "dark" }}
+            >
+              Language
+            </InputLabel>
+            <Select
+              sx={{
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+                "& .MuiSelect-select": {
+                  display: "flex",
+                  alignItems: "center",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  color: "white",
+                },
+              }}
+            >
+              <MenuItem
+                sx={{
+                  
+                  color: "black",
+                  "&.MuiMenuItem-root": {
+                    backgroundColor: "green",
+                    width: 120,
+                  },
+                  "&.Mui-selected": {
+                    backgroundColor: "blue",
+                  },
+                  "&.Mui-focusVisible": {
+                    backgroundColor: "black",
+                  },
+                }}
+                value={"deutsch"}
+              >
+                Deutsch
+                <Avatar
+                  sx={{
+                    ml: 2,
+                    width: 20,
+                    height: 18,
+                  }}
+                  src="src/assets/Flag_of_Germany.png"
+                />{" "}
+              </MenuItem>
+              <MenuItem
+                sx={{
+                  color: "black",
+                  "&.MuiMenuItem-root": {
+                    backgroundColor: "green",
+                    width: 180,
+                  },
+                  "&.Mui-selected": {
+                    backgroundColor: "blue",
+                  },
+                  "&.Mui-focusVisible": {
+                    backgroundColor: "black",
+                  },
+                }}
+                value={"english"}
+              >
+                English
+                <Avatar
+                  sx={{
+                    ml: 2,
+                    width: 20,
+                    height: 18,
+                  }}
+                  src="src/assets/Flag_of_the_United_Kingdom.png"
+                />{" "}
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
       </Stack>
     </Stack>
   );
 };
 
 export default Footer;
+
+{
+  /* <Box display={"flex"} flexDirection={"column"}>
+<Chip
+  avatar={<Avatar src="src/assets/Flag_of_Germany.png" />}
+  label="Deutsch"
+  sx={{ width: 100, bgcolor: "yellow" }}
+/>
+<Chip
+  avatar={
+    <Avatar src="src/assets/Flag_of_the_United_Kingdom.png" />
+  }
+  label="English"
+  sx={{ width: 100, bgcolor: "green" }}
+/>
+</Box> */
+}
