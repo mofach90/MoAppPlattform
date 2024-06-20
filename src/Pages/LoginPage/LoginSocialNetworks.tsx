88d634e5d835716d5f3ed1611bc5bbedb4efc1a5
mofach90 <ayari.mohamed@web.de>
2024-06-20 15:18:19 +0200

    Add facebook button

--- src/Pages/LoginPage/LoginSocialNetworks.tsx
+++ src/Pages/LoginPage/LoginSocialNetworks.tsx
@@ -1,13 +1,15 @@
-import { Button, Grid, Paper, Typography } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { Button, Grid, InputAdornment, Paper, TextField, Typography, colors } from '@mui/material';
 import { useAuth } from '../../contexts/authProvider';
 import { DashboardButton } from './LPComponents/goDashboardButton copy';
 import { HomeButton } from './LPComponents/goHomeButton';
+import GoogleIcon from '@mui/icons-material/Google';
 
 function LoginSocialNetworksPage() {
   const { setAuthenticationForm } = useAuth();
 
-  const handleGoogleAuthentication = () => {
-    window.open('/api/v1/auth/social-auth/google', '_self');
+  const handleSocialNetworkAuthentication = (provider: string) => {
+    window.open(`/api/v1/auth/social-auth/${provider}`, '_self');
     setAuthenticationForm('social based authentication');
   };
   return (
@@ -31,24 +33,41 @@ function LoginSocialNetworksPage() {
               padding: 4,
               display: 'flex',
               flexDirection: 'row',
-              justifyContent: 'center',
+              justifyContent: "center"
             }}
             variant="outlined"
           >
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
+            <Grid container spacing={3}               justifyContent="center"
+ >
+              <Grid item xs={12} textAlign={"center"}>
                 <Typography variant="h5">
                   Choose Your Social Network Account to Login In
                 </Typography>
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={8}>
                 <Button
-                  onClick={handleGoogleAuthentication}
+                  onClick={() => handleSocialNetworkAuthentication('google')}
                   color="success"
                   fullWidth
                   variant="contained"
+                  sx={{ marginBottom: 2 }}
                 >
-                  Log In With Google
+                  <InputAdornment position="start">
+                    <GoogleIcon fontSize='large' sx={{color:"white" }} />
+                  </InputAdornment>
+                  <Typography width={"90%"}>Log In With Google</Typography>
+                </Button>
+                <Button
+                  onClick={() => handleSocialNetworkAuthentication('facebook')}
+                  color="primary"
+                  fullWidth
+                  variant="contained"
+                  sx={{ marginBottom: 2 }}
+                >
+                  <InputAdornment  position="start"  >
+                    <FacebookIcon fontSize='large' sx={{color:"white"}} />
+                  </InputAdornment>
+                  <Typography width={"90%"}  >Log In With facebook</Typography>
                 </Button>
               </Grid>
             </Grid>
