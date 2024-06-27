78380d5544b6b00702c6a8bead94fdbd992196f0
mofach90 <ayari.mohamed@web.de>
2024-06-27 23:59:54 +0200

    Add firebaseBase config env varioables

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { FirebaseApp, initializeApp } from 'firebase/app';
 import { Auth, getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCi2iKtys-iQEzk4xyUBGSga6ZRIj5P474',
-  authDomain: 'moappplattform.firebaseapp.com',
-  projectId: 'moappplattform',
-  storageBucket: 'moappplattform.appspot.com',
-  messagingSenderId: '194275636901',
-  appId: '1:194275636901:web:6e30e526cea121ce0cff4c',
-  measurementId: 'G-1EQP137JS9',
+  apiKey: import.meta.env.VITE_API_KEY ?? '',
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN ?? '',
+  projectId: import.meta.env.VITE_PROJECT_ID ?? '',
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET ?? '',
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID ?? '',
+  appId: import.meta.env.VITE_APP_ID ?? '',
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID ?? '',
 };
 
 const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
