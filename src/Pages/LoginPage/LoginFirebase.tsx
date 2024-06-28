edf5eacceca228811db2baaec6e8662b9a1fa45e
mofach90 <ayari.mohamed@web.de>
2024-06-28 02:17:44 +0200

    Refactor Firebase login component

--- src/Pages/LoginPage/LoginFirebase.tsx
+++ src/Pages/LoginPage/LoginFirebase.tsx
@@ -1,103 +1,10 @@
 import { Grid, Paper, Typography } from '@mui/material';
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from 'firebase/auth';
-import { Form, Formik } from 'formik';
-import * as Yup from 'yup';
-import { auth } from '../../config/firebaseConfig';
-import { useAuth } from '../../contexts/authProvider';
-import ButtonWrapper from './LPComponents/ButtonWrapper';
-import TextfieldWrapper from './LPComponents/TextfieldWrapper';
+
 import { DashboardButton } from './LPComponents/goDashboardButton copy';
 import { HomeButton } from './LPComponents/goHomeButton';
-
-export type valueType = {
-  emailAdress: string;
-  password: string;
-  actionType?: string;
-};
-
-const INITIAL_FORM_STATE = {
-  emailAdress: '',
-  password: '',
-};
-const FORM_VALIDATION = Yup.object().shape({
-  emailAdress: Yup.string()
-    .required('Required Field')
-    .email('Invalid email address'),
-  password: Yup.string()
-    .required('Required FIeld')
-    .min(8, 'password is too short, -should be 8 chars minimum')
-    .matches(/^[a-zA-Z]+$/, ' must include only chars'),
-});
+import LoginFirebaseEmailPass from './LoginFirebaseComponents/LoginFirebaseEmailPass';
 
 function FirebaseLoginPage() {
-  // connectAuthEmulator(auth, 'http://127.0.0.1:8500'); // TODO Delete only for DEV
-
-  const { setAuthenticationForm } = useAuth();
-  const handleOnSubmit = async (values: valueType) => {
-    try {
-      const emailAdress: string = values.emailAdress;
-      const password: string = values.password;
-
-      console.log('values: ', values);
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        emailAdress,
-        password,
-      );
-      console.log('userCredential: ', userCredential);
-      if (userCredential.user) {
-        const idToken = await userCredential.user.getIdToken();
-        console.log('idToken: ', idToken);
-        const response: Response = await fetch(
-          '/api/v1/auth/login-firebase-email-password',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ idToken }),
-          },
-        );
-        if (response.ok) {
-          window.open('/dashboard', '_self');
-        } else {
-          console.error('failed to Validate idToken from Backend');
-        }
-      }
-    } catch (error) {
-      console.log({ error });
-    }
-  };
-  const handleOnSignUp = async (values: valueType) => {
-    try {
-      const emailAdress: string = values.emailAdress;
-      const password: string = values.password;
-
-      console.log('values: ', values);
-      const createNewuser = await createUserWithEmailAndPassword(
-        auth,
-        emailAdress,
-        password,
-      );
-      console.log('createNewuser: ', createNewuser);
-    } catch (error) {
-      console.log({ error });
-    }
-  };
-
-  const handleFormSubmit = async (values: valueType) => {
-    setAuthenticationForm(
-      'Firebase based authentication using Email and Password',
-    );
-    if (values.actionType === 'signIn') {
-      await handleOnSubmit(values);
-    } else if (values.actionType === 'signUp') {
-      await handleOnSignUp(values);
-    }
-  };
   return (
     <Grid container height="100vh">
       <Grid container alignItems={'center'} justifyContent={'end'} p={2}>
@@ -123,100 +30,46 @@ function FirebaseLoginPage() {
             }}
             variant="outlined"
           >
-            <Formik
-              validationSchema={FORM_VALIDATION}
-              initialValues={{ ...INITIAL_FORM_STATE }}
-              onSubmit={handleFormSubmit}
+            <Grid
+              container
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
             >
-              {({ setFieldValue }) => (
-                <Form style={{ width: '60%' }}>
-                  <Grid container spacing={3}>
-                    <Grid
-                      container
-                      justifyContent={'start'}
-                      alignItems={'center'}
-                      marginBottom={8}
-                      marginTop={5}
-                    >
-                      <Grid item xs={10}>
-                        <Typography
-                          variant="h5"
-                          fontWeight={'bold'}
-                          textAlign={'center'}
-                        >
-                          Login Using Firebase Authentication
-                        </Typography>
-                      </Grid>
-                      <Grid
-                        item
-                        xs={2}
-                        display={'flex'}
-                        justifyContent={'center'}
-                      >
-                        <img
-                          src="assets/firebase.png"
-                          alt="Logo"
-                          style={{ width: 50, height: 50 }}
-                        />
-                      </Grid>
-                    </Grid>
-                    <Grid
-                      container
-                      border={'1px solid'}
-                      borderRadius={4}
-                      padding={4}
-                    >
-                      <Grid item xs={12} marginBottom={4}>
-                        <Typography
-                          variant="h6"
-                          fontFamily={'monospace'}
-                          fontStyle={'oblique'}
-                        >
-                          Email and Password - Firebase Method
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12} marginBottom={2}>
-                        <Typography fontWeight={'bold'}>
-                          Please Enter you User Name and Password
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12} marginBottom={2}>
-                        <TextfieldWrapper
-                          name="emailAdress"
-                          label="Email Adresse"
-                          type="text"
-                        />
-                      </Grid>
-                      <Grid item xs={12} marginBottom={2}>
-                        <TextfieldWrapper
-                          name="password"
-                          label="Password"
-                          type="password"
-                          size="small"
-                        />
-                      </Grid>
-                      <Grid item xs={12} marginBottom={2}>
-                        <ButtonWrapper
-                          onClick={() => setFieldValue('actionType', 'signIn')}
-                        >
-                          Sign In
-                        </ButtonWrapper>
-                      </Grid>
-                      <Grid item xs={12} marginBottom={4}>
-                        <ButtonWrapper
-                          buttonProps={{
-                            sx: { bgcolor: '#34a1eb' },
-                          }}
-                          onClick={() => setFieldValue('actionType', 'signUp')}
-                        >
-                          Create New User
-                        </ButtonWrapper>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Form>
-              )}
-            </Formik>
+              <Grid
+                item
+                xs={10}
+                display={'flex'}
+                marginBottom={8}
+                marginTop={5}
+              >
+                <Grid container alignItems={'center'}>
+                  <Typography
+                    variant="h5"
+                    fontWeight={'bold'}
+                    textAlign={'center'}
+                  >
+                    Login Using Firebase Authentication
+                  </Typography>
+                </Grid>
+                <Grid container xs={2} justifyContent={'end'}>
+                  <img
+                    src="assets/firebase.png"
+                    alt="Logo"
+                    style={{ width: 50, height: 50 }}
+                  />
+                </Grid>
+              </Grid>
+              <Grid
+                item
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                xs={12}
+              >
+                <LoginFirebaseEmailPass />
+              </Grid>
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
---
+++ src/Pages/LoginPage/LoginFirebaseComponents/LoginFirebaseEmailPass.tsx
@@ -0,0 +1,170 @@
+import { Grid, Typography } from '@mui/material';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { auth } from '../../../config/firebaseConfig';
+import { useAuth } from '../../../contexts/authProvider';
+import ButtonWrapper from '../LPComponents/ButtonWrapper';
+import TextfieldWrapper from '../LPComponents/TextfieldWrapper';
+
+export type valueType = {
+  emailAdress: string;
+  password: string;
+  actionType?: string;
+};
+
+const INITIAL_FORM_STATE = {
+  emailAdress: '',
+  password: '',
+};
+const FORM_VALIDATION = Yup.object().shape({
+  emailAdress: Yup.string()
+    .required('Required Field')
+    .email('Invalid email address'),
+  password: Yup.string()
+    .required('Required FIeld')
+    .min(8, 'password is too short, -should be 8 chars minimum')
+    .matches(/^[a-zA-Z]+$/, ' must include only chars'),
+});
+
+function LoginFirebaseEmailPass() {
+  // connectAuthEmulator(auth, 'http://127.0.0.1:8500'); // TODO Delete only for DEV
+
+  const { setAuthenticationForm } = useAuth();
+  const handleOnSubmit = async (values: valueType) => {
+    try {
+      const emailAdress: string = values.emailAdress;
+      const password: string = values.password;
+
+      console.log('values: ', values);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        emailAdress,
+        password,
+      );
+      console.log('userCredential: ', userCredential);
+      if (userCredential.user) {
+        const idToken = await userCredential.user.getIdToken();
+        console.log('idToken: ', idToken);
+        const response: Response = await fetch(
+          '/api/v1/auth/login-firebase-email-password',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ idToken }),
+          },
+        );
+        if (response.ok) {
+          window.open('/dashboard', '_self');
+        } else {
+          console.error('failed to Validate idToken from Backend');
+        }
+      }
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+  const handleOnSignUp = async (values: valueType) => {
+    try {
+      const emailAdress: string = values.emailAdress;
+      const password: string = values.password;
+
+      console.log('values: ', values);
+      const createNewuser = await createUserWithEmailAndPassword(
+        auth,
+        emailAdress,
+        password,
+      );
+      console.log('createNewuser: ', createNewuser);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const handleFormSubmit = async (values: valueType) => {
+    setAuthenticationForm(
+      'Firebase based authentication using Email and Password',
+    );
+    if (values.actionType === 'signIn') {
+      await handleOnSubmit(values);
+    } else if (values.actionType === 'signUp') {
+      await handleOnSignUp(values);
+    }
+  };
+  return (
+    <Grid
+      container
+      display={'flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+    >
+      <Formik
+        validationSchema={FORM_VALIDATION}
+        initialValues={{ ...INITIAL_FORM_STATE }}
+        onSubmit={handleFormSubmit}
+      >
+        {({ setFieldValue }) => (
+          <Form style={{ width: '80%' }}>
+            <Grid container border={'1px solid'} borderRadius={4} padding={4}>
+              <Grid item xs={12} marginBottom={4}>
+                <Typography
+                  variant="h6"
+                  fontFamily={'monospace'}
+                  fontStyle={'oblique'}
+                >
+                  Email and Password - Firebase Method
+                </Typography>
+              </Grid>
+              <Grid item xs={12} marginBottom={2}>
+                <Typography fontWeight={'bold'}>
+                  Please Enter you User Name and Password
+                </Typography>
+              </Grid>
+              <Grid item xs={12} marginBottom={2}>
+                <TextfieldWrapper
+                  name="emailAdress"
+                  label="Email Adresse"
+                  type="text"
+                />
+              </Grid>
+              <Grid item xs={12} marginBottom={2}>
+                <TextfieldWrapper
+                  name="password"
+                  label="Password"
+                  type="password"
+                  size="small"
+                />
+              </Grid>
+              <Grid item xs={12} marginBottom={2}>
+                <ButtonWrapper
+                  onClick={() => setFieldValue('actionType', 'signIn')}
+                >
+                  Sign In
+                </ButtonWrapper>
+              </Grid>
+              <Grid item xs={12} marginBottom={4}>
+                <ButtonWrapper
+                  buttonProps={{
+                    sx: { bgcolor: '#34a1eb' },
+                  }}
+                  onClick={() => setFieldValue('actionType', 'signUp')}
+                >
+                  Create New User
+                </ButtonWrapper>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </Grid>
+  );
+}
+
+LoginFirebaseEmailPass.propTypes = {};
+
+export default LoginFirebaseEmailPass;
