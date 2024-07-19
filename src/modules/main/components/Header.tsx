b6297252614446a72449e029660686853ef9ce44
mofach90 <ayari.mohamed@web.de>
2024-07-20 00:46:09 +0200

    Add Logo props to Header

--- src/modules/main/components/Header.tsx
+++ src/modules/main/components/Header.tsx
@@ -1,15 +1,17 @@
 import { Box, Typography, useTheme } from '@mui/material';
 import { tokens } from '../../global/theme/theme';
-interface HeaderTypes {
+export interface HeaderTypes {
   title: string;
   subtitle: string;
+  imgPath?: string;
 }
 
-const Header = ({ title, subtitle }: HeaderTypes) => {
+const Header = ({ title, subtitle, imgPath }: HeaderTypes) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   return (
-    <Box mb="30px">
+    <Box mb="30px" display={'flex'} justifyContent={"space-between"} alignItems={"center"} width={"100%"}>
+      <Box>
       <Typography
         variant="h2"
         color={colors.grey[100]}
@@ -21,6 +23,9 @@ const Header = ({ title, subtitle }: HeaderTypes) => {
       <Typography variant="h5" color={colors.greenAccent[400]}>
         {subtitle}
       </Typography>
+      </Box>
+
+      <img alt='To-do app image' src={imgPath} width={300} height={90}></img>
     </Box>
   );
 };
