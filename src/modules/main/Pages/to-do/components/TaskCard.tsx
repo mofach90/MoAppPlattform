f67865c1c220e609530ad8b2a816b238765331fe
mofach90 <ayari.mohamed@web.de>
2024-07-20 01:29:06 +0200

    Add Task Card

---
+++ src/modules/main/Pages/to-do/components/TaskCard.tsx
@@ -0,0 +1,22 @@
+import { Paper, Theme, useTheme } from '@mui/material';
+import { tokens } from '../../../../global/theme/theme';
+
+const TaskCard = ({ children }: { children: React.ReactNode }) => {
+  const theme: Theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Paper
+      sx={{
+        backgroundColor: 'white',
+        width: '90%',
+        padding: 5,
+        height: 300,
+        borderRadius: 9,
+      }}
+    >
+      {children}
+    </Paper>
+  );
+};
+
+export default TaskCard;
