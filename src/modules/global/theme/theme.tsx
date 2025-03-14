ad8a0dd38d973b7208a1d8e44c1e9ff3566edcbe
mofach90 <ayari.mohamed@web.de>
2025-03-14 23:43:44 +0100

    Update theme

--- src/modules/global/theme/theme.tsx
+++ src/modules/global/theme/theme.tsx
@@ -185,55 +185,6 @@ export const tokens = (mode: Mode): colorLibrary => ({
       }),
 });
 
-// mui theme setting
-
-interface themeSettingInterface {
-  palette: {
-    primary: {
-      main: string;
-    };
-    secondary: {
-      main: string;
-    };
-    neutral: {
-      dark: string;
-      main: string;
-      light: string;
-    };
-    background: {
-      default: string;
-    };
-    mode: Mode;
-  };
-  typography: {
-    fontFamily: string;
-    fontSize: number;
-    h1: {
-      fontFamily: string;
-      fontSize: number;
-    };
-    h2: {
-      fontFamily: string;
-      fontSize: number;
-    };
-    h3: {
-      fontFamily: string;
-      fontSize: number;
-    };
-    h4: {
-      fontFamily: string;
-      fontSize: number;
-    };
-    h5: {
-      fontFamily: string;
-      fontSize: number;
-    };
-    h6: {
-      fontFamily: string;
-      fontSize: number;
-    };
-  };
-}
 
 const themeSetting = (mode: Mode) => {
   const colors = tokens(mode);
